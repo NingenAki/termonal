1bc7da471a46ceedf5adfb78d626c88e06e58452
luiza.rosa <luiza.rosa@ipqm.mb>
2025-12-10 15:22:18 -0300

    init

---
+++ src/main/java/ningenaki/inc/termonal/TermonalApplication.java
@@ -0,0 +1,13 @@
+package ningenaki.inc.termonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermonalApplication.class, args);
+	}
+
+}
