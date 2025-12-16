d107a0f1484f74902e1130725c87a6f8f18d69b0
NingenAki <luizamorgg@gmail.com>
2025-12-16 16:18:13 -0300

    refatoracao e implementacao do blink do cursor

--- src/main/java/ningenaki/inc/termonal/services/Window.java
+++ src/main/java/ningenaki/inc/termonal/services/Window.java
@@ -13,7 +13,6 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -30,70 +29,82 @@ public class Window {
     int WORD_SIZE = 5;
     int TRIES = 6;
 
+    long lastBlink = 0;
+    boolean blink = true;
+
     @Autowired
     private Words words;
 
     private MatrixStream matrixStream = new MatrixStream(WIDTH, HEIGHT);
     private Box box = new Box(WORD_SIZE, TRIES);
 
-    @PostConstruct
-    private void init() {
+    public Window() {
         box.setOrigin((WIDTH - box.getWidth()) / 2, (HEIGHT - box.getHeight()) / 2);
+        terminal = new DefaultTerminalFactory().setTerminalEmulatorTitle("Termonal")
+                .setInitialTerminalSize(new TerminalSize(WIDTH, HEIGHT))
+                .createTerminalEmulator();
+    }
+
+    public void getInput() throws IOException {
+        KeyStroke keyStroke = terminal.pollInput();
+        if (keyStroke != null) {
+            log.info(keyStroke.toString());
+            switch (keyStroke.getKeyType()) {
+                case KeyType.EOF:
+                    System.exit(0);
+                    break;
+                case KeyType.Character:
+                    box.setLetter(keyStroke.getCharacter(), cursor_x, cursor_y);
+                case KeyType.ArrowRight:
+                    if (cursor_x + 1 < WORD_SIZE)
+                        cursor_x++;
+                    break;
+                case KeyType.Backspace:
+                    box.setLetter(null, cursor_x, cursor_y);
+                case KeyType.ArrowLeft:
+                    if (cursor_x > 0)
+                        cursor_x--;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void draw() throws IOException {
+        if (System.currentTimeMillis() - lastBlink > 500) {
+            lastBlink = System.currentTimeMillis();
+            blink = !blink;
+        }
+        terminal.clearScreen();
+        terminal.setBackgroundColor(TextColor.ANSI.BLACK);
+        terminal.setForegroundColor(TextColor.ANSI.GREEN);
+        for (int y = 0; y < HEIGHT; y++) {
+            terminal.setCursorPosition(0, y);
+            for (int x = 0; x < WIDTH; x++) {
+                if (box.isIn(y, x)) {
+                    if (blink && box.offsetX(cursor_x) == x && box.offsetY(cursor_y) == y) {
+                        terminal.setBackgroundColor(TextColor.ANSI.GREEN);
+                        terminal.setForegroundColor(TextColor.ANSI.BLACK);
+                        terminal.putCharacter(box.getChar(x, y));
+                        terminal.setBackgroundColor(TextColor.ANSI.BLACK);
+                        terminal.setForegroundColor(TextColor.ANSI.GREEN);
+                    } else {
+                        terminal.putCharacter(box.getChar(x, y));
+                    }
+                } else
+                    terminal.putCharacter(matrixStream.get(x, y));
+            }
+        }
+        terminal.flush();
     }
 
     @Scheduled(fixedRate = 100)
-    public void draw() {
+    private void update() {
         try {
-            if (terminal == null)
-                terminal = new DefaultTerminalFactory().setTerminalEmulatorTitle("Termonal")
-                        .setInitialTerminalSize(new TerminalSize(WIDTH, HEIGHT))
-                        .createTerminalEmulator();
-
             matrixStream.update();
-            KeyStroke keyStroke = terminal.pollInput();
-            if (keyStroke != null) {
-                log.info(keyStroke.toString());
-                switch (keyStroke.getKeyType()) {
-                    case KeyType.EOF:
-                        System.exit(0);
-                        break;
-                    case KeyType.Character:
-                        box.setLetter(keyStroke.getCharacter(), cursor_x, cursor_y);
-                    case KeyType.ArrowRight:
-                        if (cursor_x + 1 < WORD_SIZE)
-                            cursor_x++;
-                        break;
-                    case KeyType.Backspace:
-                        box.setLetter(null, cursor_x, cursor_y);
-                    case KeyType.ArrowLeft:
-                        if (cursor_x > 0)
-                            cursor_x--;
-                        break;
-                    default:
-                        break;
-                }
-            }
-            terminal.clearScreen();
-            terminal.setBackgroundColor(TextColor.ANSI.BLACK);
-            terminal.setForegroundColor(TextColor.ANSI.GREEN);
-            for (int y = 0; y < HEIGHT; y++) {
-                terminal.setCursorPosition(0, y);
-                for (int x = 0; x < WIDTH; x++) {
-                    if (box.isIn(y, x)) {
-                        if (box.offsetX(cursor_x) == x && box.offsetY(cursor_y) == y) {
-                            terminal.setBackgroundColor(TextColor.ANSI.GREEN);
-                            terminal.setForegroundColor(TextColor.ANSI.BLACK);
-                            terminal.putCharacter(box.getChar(x, y));
-                            terminal.setBackgroundColor(TextColor.ANSI.BLACK);
-                            terminal.setForegroundColor(TextColor.ANSI.GREEN);
-                        } else {
-                            terminal.putCharacter(box.getChar(x, y));
-                        }
-                    } else
-                        terminal.putCharacter(matrixStream.get(x, y));
-                }
-            }
-            terminal.flush();
+            getInput();
+            draw();
         } catch (IOException ex) {
             log.error("Erro ao printar no termonal", ex);
         }
