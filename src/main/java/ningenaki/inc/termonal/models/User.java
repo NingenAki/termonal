2a1c1e1d20102f2f215acf6114830bb8f4ce217d
NingenAki <luizamorgg@gmail.com>
2026-09-18 15:24:57 -0300

    add database on supabase

---
+++ src/main/java/ningenaki/inc/termonal/models/User.java
@@ -0,0 +1,26 @@
+package ningenaki.inc.termonal.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(generator = "user_id_seq", strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 50, nullable = false, unique = true)
+    @EqualsAndHashCode.Include
+    private String username;
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+}
